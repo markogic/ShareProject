7522d36c922f872ebb24756b62ecb94ae7025a24
Marko Gicevski <markogic@hotmail.com>
2013-05-08 16:22:59 +0200

    add new share project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShareProject::Application.config.secret_token = '4268cf3920d04d324b9264535e42d3fce0964b78b009d737968afebac923e8f6314381cbb85dc3c0b15fedc64f1523a96881ba1eed3d892da90b3ffeaf8805d0'
